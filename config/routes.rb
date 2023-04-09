db7b83e1ed7e4a3dfb25cb9396d488731c37461f
mof-devops <info.ifmisu@gmail.com>
2023-04-09 23:16:41 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  #get 'resumes/index'
+  #get 'resumes/new'
+  #get 'resumes/create'
+  #get 'resumes/destroy'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  resources :resumes, only: [:index, :new, :create, :destroy]
+  root "resumes#index"
+
+end
